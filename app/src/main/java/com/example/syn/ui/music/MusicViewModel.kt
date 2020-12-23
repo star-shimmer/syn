86d8c1c33d4796d4ecc80c5272021429e4e212e8
Your Name <you@example.com>
2020-12-23 21:05:27 +0800

    the thrid

--- app/src/main/java/com/example/syn/ui/music/MusicViewModel.kt
+++ app/src/main/java/com/example/syn/ui/music/MusicViewModel.kt
@@ -1,10 +1,12 @@
 package com.example.syn.ui.music
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class MusicViewModel : ViewModel() {
+class MusicViewModel(application: Application) : AndroidViewModel(application) {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is music Fragment"
